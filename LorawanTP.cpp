4bf3f0fa81fe04e0277e11766202d5ad08774005
Rafaella Neofytou <rafaella@think-engineer.com>
2019-10-01 17:30:58 +0100

    Add empty lorawan lib files

---
+++ LorawanTP.cpp
@@ -0,0 +1,12 @@
+/**
+  * @file    LorawanTP.cpp
+  * @version 1.0.0
+  * @author  
+  * @brief   C++ file of the SX1276 driver module. 
+  * Handles communication to the thingpilot API utilising the SX1272/SX1276 (or compatible) modem
+  */
+
+/** Includes
+ */
+#include "LorawanTP.h"
+
---
+++ LorawanTP.h
@@ -0,0 +1,16 @@
+/**
+  * @file    LorawanTP.h
+  * @version 1.0.0
+  * @author  
+  * @brief   Header file of the SX1276 driver module. 
+  * Handles communication to the thingpilot API utilising the SX1272/SX1276 (or compatible) modem
+  */
+
+/** Define to prevent recursive inclusion
+ */
+#pragma once
+
+/** Includes 
+ */
+#include <mbed.h>
+
